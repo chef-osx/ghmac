5d72d6d58760f4cd4490a339f0d4c10042073247
jtimberman <joshua@housepub.org>
2011-06-22 22:43:59 -0600

    initial commit

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+default['ghmac']['version'] = '1.0'
+default['ghmac']['url'] = "https://github-central.s3.amazonaws.com/mac/GitHub%20for%20Mac%20#{ghmac['version']}.zip"
+default['ghmac']['checksum'] = '7ed847e0484c748e1ee14894c33dd0cb3eba129af5a3efce04291e25a87fa242'
+default['ghmac']['install_to'] = "/Applications"

5d7e015eff3fb8a79bfa1e74139598307315df77
jtimberman <joshua@opscode.com>
2011-11-27 11:17:46 -0700

    ghmac v1.1.0

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,22 @@
-default['ghmac']['version'] = '1.0'
+#
+# Author:: Joshua Timberman <opensource@housepub.org>
+# Copyright:: Copyright (c) 2011, Joshua Timberman
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+default['ghmac']['version'] = '1.1'
 default['ghmac']['url'] = "https://github-central.s3.amazonaws.com/mac/GitHub%20for%20Mac%20#{ghmac['version']}.zip"
-default['ghmac']['checksum'] = '7ed847e0484c748e1ee14894c33dd0cb3eba129af5a3efce04291e25a87fa242'
+default['ghmac']['checksum'] = "8c3d2c0be3c6a63d2e7baa1f36e8da83c6c960292b2a1de940afce804d508252"
 default['ghmac']['install_to'] = "/Applications"
